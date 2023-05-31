9a096d2bfbc3d8b1955d9df644949acba273bc5f
DONGGEUN JUNG <40321421+DonggeunJung@users.noreply.github.com>
2023-05-31 17:34:28 -0400

    Create StringEx.py

---
+++ basic/StringEx.py
@@ -0,0 +1,12 @@
+print('Hello')
+# Hello
+print("I love the Earth!")
+I love the Earth!
+print("My favorite numbers are 1, 4, and 7")
+# My favorite numbers are 1, 4, and 7
+city = 'Seoul'
+future = "Future city"
+print(future)
+# Future city
+print(city)
+# Seoul
