93d3615416be70338e7dea5b3456d79b848b830c
DONGGEUN JUNG <40321421+DonggeunJung@users.noreply.github.com>
2023-05-31 17:32:51 -0400

    Create CalculateEx.py

---
+++ basic/CalculateEx.py
@@ -0,0 +1,22 @@
+add = 3 + 25
+sub = 45 - 13
+mul = 9 * 9
+div = 3 / 2
+div_init = 3 // 2
+exp = 10 ** 3
+mod = 101 % 10
+
+print(add)
+# 28
+print(sub)
+# 32
+print(mul)
+# 81
+print(div)
+# 1.5
+print(div_init)
+# 1
+print(exp)
+# 1000
+print(mod)
+# 1
