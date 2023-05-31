f0a8d3f90d9af0927aeee0445b215a8c46c73e39
DONGGEUN JUNG <40321421+DonggeunJung@users.noreply.github.com>
2023-05-31 17:37:47 -0400

    Create MultipleLines.py

---
+++ basic/MultipleLines.py
@@ -0,0 +1,21 @@
+number = 1
+print(number)
+# 1
+num1 = 10; num2 = 20; num3 = 30
+print(num1 + num2 + num3)
+# 60
+sum = 1 + 2 + 3 + \
+      4 + 5 + 6 + \
+      7 + 8 + 9
+print(sum)
+# 45
+tup = (1 + 2 + 3 +
+       4 + 5 + 6 +
+       7 + 8 + 9)
+print(tup)
+# 45
+color = ['red',
+         'blue',
+         'green']
+print(color)
+# ['red', 'blue', 'green']
