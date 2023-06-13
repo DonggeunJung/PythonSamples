a0f06d27c40a81216ccf8615c86b3c17c46aa33d
DONGGEUN JUNG <40321421+DonggeunJung@users.noreply.github.com>
2023-06-12 20:20:45 -0400

    Create Fish1.py

---
+++ ml/Fish1.py
@@ -0,0 +1,31 @@
+import pandas as pd
+fish = pd.read_csv('./docs/fish.csv')
+# print(fish.head())
+# print(pd.unique(fish['Species']))
+fish_input = fish[['Weight', 'Length', 'Diagonal', 'Height', 'Width']].to_numpy()
+fish_target = fish['Species'].to_numpy()
+# print(fish_input[:5])
+
+from sklearn.model_selection import train_test_split
+train_input, test_input, train_target, test_target = train_test_split(fish_input,
+                                            fish_target, random_state=42)
+
+from sklearn.preprocessing import StandardScaler
+ss = StandardScaler()
+ss.fit(train_input)
+train_scaled = ss.transform(train_input)
+test_scaled = ss.transform(test_input)
+
+from sklearn.neighbors import KNeighborsClassifier
+kn = KNeighborsClassifier(n_neighbors=3)
+kn.fit(train_scaled, train_target)
+# print(kn.score(train_scaled, train_target))
+# print(kn.score(test_scaled, test_target))
+# print(kn.classes_)
+
+import numpy as np
+proba = kn.predict_proba(test_scaled[:5])
+# print(np.round(proba, decimals=4))
+
+distances, indexes = kn.kneighbors(test_scaled[3:4])
+print(train_target[indexes])
